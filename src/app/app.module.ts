88ce31e45a4c965038a199570794c83b0c25e748
alessioportacci <alessio.portacci.s@gmail.com>
2023-07-24 15:49:57 +0200

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Header1Component } from './header1/header1.component';
+import { Header2Component } from './header2/header2.component';
+import { Header3Component } from './header3/header3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Header1Component,
+    Header2Component,
+    Header3Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header3/header3.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header3',
+  templateUrl: './header3.component.html',
+  styleUrls: ['./header3.component.scss']
+})
+export class Header3Component {
+
+}
